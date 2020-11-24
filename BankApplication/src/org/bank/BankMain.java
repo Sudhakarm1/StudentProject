de754633438bb00aac781165382fe595158ca811
iamsudhakar <sudhakarm761998@gmail.com>
2020-11-24 14:33:39 +0530

    Application1.3

--- BankApplication/src/org/bank/BankMain.java
+++ BankApplication/src/org/bank/BankMain.java
@@ -8,9 +8,9 @@ public class BankMain {
 	{
 		Scanner Sc=new Scanner(System.in);
 		System.out.println("Choose your bank");
-		System.out.println("1.HDFC Bank");
-		System.out.println("2.SBI Bank");
-		System.out.println("3.ICICI Bank");
+		System.out.println("1.HDFC Bank ");
+		System.out.println("2.SBI Bank ");
+		System.out.println("3.ICICI Bank ");
 	    int Bank=Sc.nextInt();
 	    switch(Bank)
 	    {
